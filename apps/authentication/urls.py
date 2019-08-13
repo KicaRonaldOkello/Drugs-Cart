69d4c7937d7e21d5856caa9916762177d286b92a
KicaRonaldOkello <okello.ronald@gmail.com>
2019-08-13 19:29:12 +0300

    ft(login): Add login
    - Add login functionality for the application
    
    [Delivers #167823947]

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import (SignUpView, LoginUserView)
 
 urlpatterns = [
-    path('auth/signup/', SignUpView.as_view(), name='signup')
+    path('auth/signup/', SignUpView.as_view(), name='signup'),
+    path('auth/login/', LoginUserView.as_view(), name='login')
 ]
